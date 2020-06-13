e93f2481e71e53019842601f0556cb4d7665bc45
sean-slaughter <sthompson2822@gmail.com>
2020-06-13 15:36:35 -0500

    Done.

---
+++ lib/players/human.rb
@@ -0,0 +1,17 @@
+module Players
+
+    class Human < Player
+
+        
+        def move(board)
+         puts "Input number between 1-9"
+         input = gets.strip
+         input
+        end
+
+    end
+
+    
+
+end
+
